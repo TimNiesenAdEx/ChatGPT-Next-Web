388a0a83f029eb7c92fc0af68ab4d0a2c5d0cb54
Tim Niesen <tim.niesen@adexpartners.com>
2023-06-20 14:47:18 +0200

    remove cn prompts

--- scripts/fetch-prompts.mjs
+++ scripts/fetch-prompts.mjs
@@ -8,7 +8,7 @@ const RAW_CN_URL = "PlexPt/awesome-chatgpt-prompts-zh/main/prompts-zh.json";
 const CN_URL = MIRRORF_FILE_URL + RAW_CN_URL;
 const RAW_EN_URL = "f/awesome-chatgpt-prompts/main/prompts.csv";
 const EN_URL = MIRRORF_FILE_URL + RAW_EN_URL;
-const ADEX_URL = "https://raw.githubusercontent.com/TimNiesenAdEx/ChatGPT-Next-Web/main/public/adex_prompts.csv"; 
+const ADEX_URL = "https://raw.githubusercontent.com/TimNiesenAdEx/ChatGPT-Next-Web/main/public/adex_prompts.csv";
 const FILE = "./public/prompts.json";
 
 const ignoreWords = ["涩涩", "魅魔"];
