c7b79a92ea74411a0375c0a84aa8bb48288e5b06
Tim Niesen <tim.niesen@adexpartners.com>
2023-06-20 13:47:40 +0200

    remove cn prompts

--- scripts/fetch-prompts.mjs
+++ scripts/fetch-prompts.mjs
@@ -9,7 +9,7 @@ const CN_URL = MIRRORF_FILE_URL + RAW_CN_URL;
 const RAW_EN_URL = "f/awesome-chatgpt-prompts/main/prompts.csv";
 const EN_URL = MIRRORF_FILE_URL + RAW_EN_URL;
 const FILE = "./public/prompts.json";
-const ADEX_PROMPTS = "https://raw.githubusercontent.com/TimNiesenAdEx/ChatGPT-Next-Web/e05ea5c29bbbb4c201482974bb733ad995a8c8bd/public/adex_prompts.csv";
+const ADEX_PROMPTS = "https://www.dropbox.com/s/ojbu9rmnq3osvjy/adex_prompts.csv";
 
 const ignoreWords = ["涩涩", "魅魔"];
 
@@ -61,35 +61,14 @@ async function fetchEN() {
   }
 }
 
-async function fetchAdEx() {
-  console.log("[Fetch] fetching AdEx prompts...");
-  try {
-    // const raw = await (await fetch(EN_URL)).text();
-    const response = await Promise.race([fetch(ADEX_PROMPTS), timeoutPromise(5000)]);
-    const raw = await response.text();
-    return raw
-      .split("\n")
-      .slice(1)
-      .map((v) =>
-        v
-          .split('","')
-          .map((v) => v.replace(/^"|"$/g, "").replaceAll('""', '"'))
-          .filter((v) => v[0] && v[1]),
-      );
-  } catch (error) {
-    console.error("[Fetch] failed to fetch en prompts", error);
-    return [];
-  }
-}
-
 async function main() {
-  Promise.all([fetchEN()])
-    .then(([en]) => {
-      fs.writeFile(FILE, JSON.stringify({ en }));
+  Promise.all([fetchCN(), fetchEN()])
+    .then(([cn, en]) => {
+      fs.writeFile(FILE, JSON.stringify({ cn, en }));
     })
     .catch((e) => {
       console.error("[Fetch] failed to fetch prompts");
-      fs.writeFile(FILE, JSON.stringify({ en: [] }));
+      fs.writeFile(FILE, JSON.stringify({ cn: [], en: [] }));
     })
     .finally(() => {
       console.log("[Fetch] saved to " + FILE);
